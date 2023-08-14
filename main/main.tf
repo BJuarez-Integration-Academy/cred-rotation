d8c414237b390b2701f4c62f5b61fa798d579dab
efloresjc <everardo.flores@jumpcloud.com>
2023-08-13 21:12:47 -0600

    Comented create variables block from main.tf

--- main/main.tf
+++ main/main.tf
@@ -6,12 +6,13 @@ resource "github_actions_organization_secret" "org_secret" {
   encrypted_value = each.value["secret_value"]
 }
 
+/*
 resource "github_actions_organization_variable" "org_variable" {
   for_each = var.org_variables
   variable_name   = each.value["variable_name"]
   visibility      = "all"
   value           = each.value["variable_value"]
-}
+}*/
 #***********************ORG-REPO**************************
 resource "github_actions_secret" "org_repo_secret" {
   for_each = var.repo_secrets
@@ -19,9 +20,11 @@ resource "github_actions_secret" "org_repo_secret" {
   secret_name      = each.value["secret_name"]
   encrypted_value  = each.value["secret_value"]
 }
+
+/*
 resource "github_actions_variable" "example_variable" {
   for_each = var.repo_variables
   repository       = each.value["repo_name"]
   variable_name    = each.value["variable_name"]
   value            = each.value["variable_value"]
-}
+}*/
