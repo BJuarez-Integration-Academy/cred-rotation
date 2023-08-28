a1e6a9132fba98aa64478338cb5d47393258ecca
efloresjc <everardo.flores@jumpcloud.com>
2023-08-27 21:23:30 -0600

    Added python files and variable values as json file

---
+++ main/localModule.py
@@ -0,0 +1,29 @@
+import json, os,requests
+
+personalAccessToken = os.environ['POC_GIT_PAT']
+ORG_NAME = os.environ['POC_GIT_OWNER']
+
+################################-Initializing global vars-##############################################
+token = "Bearer " + personalAccessToken
+header = {
+    "X-GitHub-Api-Version": "2022-11-28",
+    "Authorization": token
+}
+        
+#################################-Trigger Workflow Function-#################################
+def triggerWorkflow(repoName: str,env:str):
+    url = "https://api.github.com/repos/"+ORG_NAME+"/"+repoName+"/actions/workflows"
+    getResp = requests.get(url, headers = header)
+    if getResp.status_code == 200:
+        print("===>Triggering ["+repoName+ "] repo...")
+        getResp = json.loads(getResp.text) 
+        wfId=getResp['workflows'][0]['id']
+        url = "https://api.github.com/repos/"+ORG_NAME+"/"+repoName+"/actions/workflows/"+str(wfId)+"/dispatches"
+        request = {
+            "ref": env
+        }
+        response = requests.request("POST",url,headers=header,data=json.dumps(request))
+        if response.status_code == 204 :
+            print("====> Status: Wf Triggered Successfully")
+        else: print("Error while triggering the workflow from repo: "+ repoName)
+    else: print("Error while getting workflow ID from repo: "+ repoName)
---
+++ main/main.py
@@ -0,0 +1,22 @@
+import json,os,glob
+import localModule as lm
+
+rpath = os.environ['GITHUB_ROOT_DIR']
+
+#Getting the .json file name
+files = glob.glob("*.json")
+
+#Getting the environment from the file name
+env = files[0].split("_")
+env = env[1].split(".")
+env = env[0]
+print("\n====>Executing on [" + env + "] environment\n")
+print("Processing [" +files[0]+"] file")
+
+#Reading decrypted file
+with open(rpath + "/"+files[0]) as f:
+    payload = json.load(f)
+        
+#Triggering git actions workflows
+for repo in payload['reposToTrigger']:
+    lm.triggerWorkflow(repo,env)
